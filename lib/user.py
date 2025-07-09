632916be526d04999facd6569a631767d3c3bc7a
khadijun@DESKTOP <kardikhadija1@gmail.com>
2025-07-09 10:25:44 +0300

    done

--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
-
 class User:
-    pass
+    def __init__(self, first_name, last_name, knowledge=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        if knowledge is None:
+            self.knowledge = []
+        else:
+            self.knowledge = knowledge
